20563a0c3c68c5370aba6fdf75cc63bb5b533d74
Harry Alexander <harryalexander1499@gmail.com>
2023-06-27 18:33:35 +0100

    Added code to generate icosahedron

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -1,48 +1,114 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Mesh mesh;
-    public Vector3[] vertices;
-    public int[] triangles;
-    void Awake()
+    public Vector3[] meshVertices;
+    public int[] meshTriangles;
+    public Mesh mesh;
+
+    private void Awake()
     {
-        // Assuming you have a reference to the mesh you want to work with
-        mesh = GetComponent<MeshFilter>().mesh;
-
-        // Get the array of vertices
-        vertices = mesh.vertices;
-
-        // Loop through each vertex
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            // Access individual vertices
-            string vertexPosition = vertices[i].ToString();
-            Debug.Log(vertexPosition);
-            if (i == 100 || i == 101 || i == 102) {
-                vertices[i] = new(vertices[i].x + 3, vertices[i].y + 3, vertices[i].z + 3);
-            }
-        }
-        Debug.Log(vertices.Length);
+        CreateMesh();
         UpdateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
+    // generate icosphere
+    public class Icosahedron{
+
+        float t = (float)((1f + Math.Sqrt(5f)) / 2f);
+        private List<Vector3> _vertices = new();
+
+        public Icosahedron() {
+            GenerateVertices();
+        }
+
+        private void GenerateVertices() {
+
+            Vector3 vertex = new(-1, t, 0);
+            Vector3 vertex1 = new(1, t, 0);
+            Vector3 vertex2 = new(-1, -t, 0);
+            Vector3 vertex3 = new(1, -t, 0);
+
+            Vector3 vertex4 = new(0, -1, t);
+            Vector3 vertex5 = new(0, 1, t);
+            Vector3 vertex6 = new(0, -1, -t);
+            Vector3 vertex7 = new(0, 1, -t);
+
+            Vector3 vertex8 = new(t, 0, -1);
+            Vector3 vertex9 = new(t, 0, 1);
+            Vector3 vertex10 = new(-t, 0, -1);
+            Vector3 vertex11 = new(-t, 0, 1);
+
+            _vertices.Add(vertex);
+            _vertices.Add(vertex1);
+            _vertices.Add(vertex2);
+            _vertices.Add(vertex3);
+            _vertices.Add(vertex4);
+            _vertices.Add(vertex5);
+            _vertices.Add(vertex6);
+            _vertices.Add(vertex7);
+            _vertices.Add(vertex8);
+            _vertices.Add(vertex9);
+            _vertices.Add(vertex10);
+            _vertices.Add(vertex11);
+        }
+
+        public List<Vector3> Vertices
+        {
+            get { return _vertices; }
+        }
+    }
+    public class Icosphere { 
+
     }
 
+    // displace vertices with perlin noise
+
+    // Create mesh
+    void CreateMesh() {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Icosahedron icosahedron = new Icosahedron();
+        meshVertices = icosahedron.Vertices.ToArray();
+        meshTriangles = new int[]
+        {
+            0,11,5,
+            0,5,1,
+            0,1,7,
+            0,7,10,
+            0,10,11,
+
+            1,5,9,
+            5,11,4,
+            11,10,2,
+            10,7,6,
+            7,1,8,
+
+            3,9,4,
+            3,4,2,
+            3,2,6,
+            3,6,8,
+            3,8,9,
+
+            4,9,5,
+            2,4,11,
+            6,2,10,
+            8,6,7,
+            9,8,1
+        };
+    }
     void UpdateMesh()
     {
         mesh.Clear();
-        mesh.vertices = vertices;
-        triangles = mesh.triangles;
-        // Assign the UV coordinates to the mesh
-        //mesh.uv = uvCoordinates;
+        mesh.vertices = meshVertices;
+        mesh.triangles = meshTriangles;
         mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
     }
+
+    // map textures to terrain
 }
